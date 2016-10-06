1d0761163ed1db62ca0ac5ee07b5e2862c689864
Diego Alves <d14l.diego@gmail.com>
2016-10-06 14:49:41 -0300

    up

---
+++ app/src/main/java/com/mdeveloper/diegoalves/cars/util/Connection.java
@@ -0,0 +1,8 @@
+package com.mdeveloper.diegoalves.cars.util;
+
+/**
+ * Created by Diego Alves on 22/09/2016.
+ */
+
+public class Connection {
+}
---
+++ app/src/main/java/com/mdeveloper/diegoalves/cars/util/Prefs.java
@@ -0,0 +1,8 @@
+package com.mdeveloper.diegoalves.cars.util;
+
+/**
+ * Created by Diego Alves on 23/09/2016.
+ */
+
+public class Prefs {
+}
